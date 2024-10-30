3f5207443c4b20464577a1c80c116b7cff091b0b
Pramuditha Karunarathna <shemil076@gmail.com>
2024-10-30 23:28:39 +0530

    Initial Commit

---
+++ Ever or Never/Ever_or_NeverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ever_or_NeverApp.swift
+//  Ever or Never
+//
+//  Created by Pramuditha Karunarathna on 2024-10-30.
+//
+
+import SwiftUI
+
+@main
+struct Ever_or_NeverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
